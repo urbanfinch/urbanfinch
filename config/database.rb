0ac7ddc2884298aa6f894a7f2bace93563be88fd
Aaron Wright <acwrightdesign@gmail.com>
2012-01-31 13:07:04 -0600

    Updated MongoDB connection.:

--- config/database.rb
+++ config/database.rb
@@ -4,8 +4,6 @@ if ENV['MONGOLAB_URI']
   MongoMapper.database = uri.path.gsub(/^\//, '')
   MongoMapper.database.authenticate(uri.user, uri.password)
 else
-  MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-  MongoMapper.database = 'urbanfinch_' + Padrino.env
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => logger)
+  MongoMapper.database = 'urbanfinch_' + Padrino.env.to_s
 end
-
-MongoMapper.logger = logger
