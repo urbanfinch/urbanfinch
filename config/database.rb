e48119e3d898081a5e5d7044c4a226ea6a4bb1fd
Aaron Wright <acwrightdesign@gmail.com>
2012-01-31 13:29:00 -0600

    Trying fix...

--- config/database.rb
+++ config/database.rb
@@ -1,9 +1,9 @@
 if ENV['MONGOLAB_URI']
   uri = URI.parse(ENV['MONGOLAB_URI'])
-  MongoMapper.connection = Mongo::Connection.new(uri.host, uri.port)
+  MongoMapper.connection = Mongo::Connection.new(uri.host, uri.port, :logger => logger)
   MongoMapper.database = uri.path.gsub(/^\//, '')
   MongoMapper.database.authenticate(uri.user, uri.password)
 else
-  MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => logger)
   MongoMapper.database = 'urbanfinch_' + Padrino.env.to_s
 end
